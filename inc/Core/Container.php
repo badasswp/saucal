4891d98afeb42d0c8792ce07acf6e34151f164c3
badasswp <badasswpdev@gmail.com>
2024-11-08 00:57:01 +0100

    chore: implement Container factory

---
+++ inc/Core/Container.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Container Class.
+ *
+ * This class acts as a Factory class for registering
+ * all Plugin services.
+ *
+ * @package Saucal
+ */
+
+namespace Saucal\Core;
+
+use Saucal\Services\Boot;
+use Saucal\Interfaces\Kernel;
+
+class Container implements Kernel {
+	/**
+	 * Services.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var array
+	 */
+	public static array $services;
+
+	/**
+	 * Set up Services.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct() {
+		static::$services = [
+			Boot::class,
+		];
+	}
+
+	/**
+	 * Register Services.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public function register(): void {
+		foreach ( static::$services as $service ) {
+			( $service::get_instance() )->register();
+		}
+	}
+}
